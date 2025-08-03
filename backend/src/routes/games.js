d9ab8acb39a2857c510777a5cc265f30d06d60fb
lalacarvalh <lays.cruz@jala.university>
2025-08-03 02:36:42 -0300

    Implement leave game, end game, get game status, and players list features.

--- backend/src/routes/games.js
+++ backend/src/routes/games.js
@@ -10,5 +10,10 @@ router.delete("/games/:id", handler.deleteGame);
 router.post("/games/joinGame", handler.joinGame);
 router.post("/games/markAsReady", handler.markAsReady);
 router.post("/games/startGame", handler.startGame);
+router.post("/games/leave", handler.leaveGame); //8
+router.post("/games/end", handler.endGame); //9
+router.post("/games/state", handler.getGameState); //10
+router.post("/games/players", handler.getPlayersInGame); //11
+
 
 module.exports = router;
