5e37879222a5c47ceb9abfcca8c54a24b060e833
lalacarvalh <lays.cruz@jala.university>
2026-01-01 03:25:53 -0300

    Mudança para MongoDB Atlas 2

--- frontend/src/api/unoApi.js
+++ frontend/src/api/unoApi.js
@@ -6,7 +6,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_API_URL  || 'http://localhost:3000',
 });
 
 export default api;
